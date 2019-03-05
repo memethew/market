a2efbcdd25f76e1f5cf61bbe88be69f9e9e54d23
1066404 <1066404@BVH-202-306441.SUHSD.NET>
2019-03-05 07:21:08 -0800

    extendo

--- src/market/Cart.java
+++ src/market/Cart.java
@@ -2,12 +2,11 @@ package market;
 
 import java.util.HashMap;
 
-public class Cart {
+public class Cart extends Library{
 
 	private double total;
-	private HashMap<String, Double> cart;
 	
 	public Cart() {
-		cart = new HashMap<>();
+		
 	}
 }
