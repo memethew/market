e9f44e21a7a363d60f6e1426d98236baee37c6d6
1067763 <1067763@BVH-202-306440.SUHSD.NET>
2019-03-20 08:00:39 -0700

    isdafjhodkjf

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -1,6 +1,41 @@
 package employee;
-
+import java.util.Date;
 public abstract class Employee {
-
+	private String name;
+	private int age;
+	private Date dateCreated;
 	
+	protected Employee(String name, int age) {
+		this.name = name;
+		this.age = age;
+		this.dateCreated = new Date();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	
+	public abstract short calculateSalary();
+	public abstract long calculateBenefits();
+	public abstract float calculateChanceToBeFired();
 }
